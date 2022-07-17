faff0d6b7043ab5f5e7b5e265a16d41bd79e63ff
Rohan Lamb <lambrohan@gmail.com>
2022-07-17 22:09:03 +0530

    chore: web3 microservice created

---
+++ apps/web3/src/main.ts
@@ -0,0 +1,22 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { RedisTransportConfig } from '@metacoins/shared';
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
+
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    RedisTransportConfig
+  );
+  await app.listen();
+  Logger.log(`Web3 Microservice Started`);
+}
+
+bootstrap();
