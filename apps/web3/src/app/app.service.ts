9b832f946835391e37b31629017e96bead6da7f7
Rohan Lamb <lambrohan@gmail.com>
2022-07-18 15:24:37 +0530

    feat: add prisma to web3 service

--- apps/web3/src/app/app.service.ts
+++ apps/web3/src/app/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../common/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) {}
   getData(): { message: string } {
     return { message: 'Welcome to web3!' };
   }
